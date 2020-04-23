df3e51e7579aee49980905ed1531b9780bd9a974
Martin Brossard <martin.brossard@mines-paristech.fr>
2020-04-23 12:26:40 +0200

    Update networks.py

--- src/networks.py
+++ src/networks.py
@@ -83,3 +83,44 @@ class GyroNet(BaseNet):
         Rot_us = bbmv(Rots, us[:, :, :3])
         return self.gyro_std*ys.transpose(1, 2) + Rot_us
 
+
+class GyroNetWithoutAcc(BaseNet):
+    def __init__(self, in_dim, out_dim, c0, dropout, ks, ds, momentum,
+        gyro_std):
+        super().__init__(in_dim, out_dim, c0, dropout, ks, ds, momentum)
+        gyro_std = torch.Tensor(gyro_std)
+        self.gyro_std = torch.nn.Parameter(gyro_std, requires_grad=False)
+
+        gyro_Rot = 0.05*torch.randn(3, 3).cuda()
+        self.gyro_Rot = torch.nn.Parameter(gyro_Rot)
+        self.Id3 = torch.eye(3).cuda()
+
+    def forward(self, us):
+        # set accelerometer to 0
+        us[:, :, 3:6] = 0
+        ys = super().forward(us)
+        Rots = (self.Id3 + self.gyro_Rot).expand(us.shape[0], us.shape[1], 3, 3)
+        Rot_us = bbmv(Rots, us[:, :, :3])
+        return self.gyro_std*ys.transpose(1, 2) + Rot_us
+    
+    
+class GyroNetWithRNN(BaseNet):
+    def __init__(self, in_dim, out_dim, c0, dropout, ks, ds, momentum,
+        gyro_std):
+        super().__init__(in_dim, out_dim, c0, dropout, ks, ds, momentum)
+        gyro_std = torch.Tensor(gyro_std)
+        self.gyro_std = torch.nn.Parameter(gyro_std, requires_grad=False)
+
+        gyro_Rot = 0.05*torch.randn(3, 3).cuda()
+        self.gyro_Rot = torch.nn.Parameter(gyro_Rot)
+        self.Id3 = torch.eye(3).cuda()
+        
+        self.lstm = torch.nn.LSTM(6, 50, 2, batch_first=True, dropout=0.1)
+
+    def forward(self, us):
+        # use LSTM
+        # ys = super().forward(us)
+        ys = self.lstm(us)[0][:, :, :3]
+        Rots = (self.Id3 + self.gyro_Rot).expand(us.shape[0], us.shape[1], 3, 3)
+        Rot_us = bbmv(Rots, us[:, :, :3])
+        return self.gyro_std*ys.transpose(1, 2) + Rot_us
